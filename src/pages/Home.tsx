9d0d1ba239df4e7d47d54cbed85b9d0b0f01a12d
nellin22 <112803065+nellin22@users.noreply.github.com>
2024-10-09 15:26:22 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,52 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+//Home page
+import { 
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonSearchbar,
+  IonSelectOption,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonLabel,
+  IonSelect
+} from '@ionic/react';
+import useApi from '../hooks/useApi';
+import { useEffect, useState } from 'react';
 
 const Home: React.FC = () => {
+  const { searchData } = useApi()
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [type, setType] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    console.log('SEARCH ', searchTerm);
+  }, [searchTerm]);
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+        <IonToolbar color={'primary'}>
+          <IonTitle>Movie App</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent>
+        <IonSearchbar value={searchTerm}
+        debounce={300} 
+        onIonChange={(e) => setSearchTerm(e.detail.value!)} />
+        
+        <IonItem>
+          <IonLabel>Select Searchtype</IonLabel>
+          <IonSelect value={type}
+          onIonChange={(e) => setType(e.detail.value!)}>
+            <IonSelectOption value="">All</IonSelectOption>
+            <IonSelectOption value="movie">Movie</IonSelectOption>
+            <IonSelectOption value="series">Series</IonSelectOption>
+            <IonSelectOption value="episode">Episode</IonSelectOption>
+          </IonSelect>
+        </IonItem>
       </IonContent>
     </IonPage>
   );
